02019fc142420e166f0e511bd05a015e1a69d1e7
Adelugba Kehinde <kennyadenat09@gmail.com>
2023-10-17 21:55:24 +0100

    updated register options

--- assets/angular/register.js
+++ assets/angular/register.js
@@ -49,24 +49,27 @@ app.controller("RegisterController", [
     $scope.sexes = ["female", "male"];
 
     $scope.addNewRegistration = function () {
-      $scope.error = "";
-      const currentYear = new Date().getFullYear().toString();
-      $scope.Registration.year = currentYear;
-      $scope.Registration.submit = "process";
+      if ($scope.addRegistration.$invalid) {
+      } else {
+        $scope.error = "";
+        const currentYear = new Date().getFullYear().toString();
+        $scope.Registration.year = currentYear;
+        $scope.Registration.submit = "process";
 
-      $http.post($scope.url, $scope.Registration).then(function (msg) {
-        $scope.message = msg.data.participants;
-        if ($scope.message.status === "true") {
-          const encodedName = encodeURIComponent($scope.message.name); // Encode the parameter for URL
-          // Navigate to another page with the parameter in the query string
-          window.location.href = `success.html?name=${encodedName}`;
-        } else {
-          $scope.Registration.submit = "submit";
-          console.log("User account already exists");
-          toastr.info("User account already exists");
-          $scope.error = $scope.message.message;
-        }
-      });
+        $http.post($scope.url, $scope.Registration).then(function (msg) {
+          $scope.message = msg.data.participants;
+          if ($scope.message.status === "true") {
+            const encodedName = encodeURIComponent($scope.message.name); // Encode the parameter for URL
+            // Navigate to another page with the parameter in the query string
+            window.location.href = `success.html?name=${encodedName}`;
+          } else {
+            $scope.Registration.submit = "submit";
+            console.log("User account already exists");
+            toastr.info("User account already exists");
+            $scope.error = $scope.message.message;
+          }
+        });
+      }
     };
   },
 ]);
